7df1d35e34ed832a98873dd8b47ac90ca58ce47f
yxchanSNsoft <59272285+yxchanSNsoft@users.noreply.github.com>
2020-08-12 14:22:45 +0800

    Add files via upload

---
+++ error.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>No page found</title>
+</head>
+<body>
+    <div style="text-align: center;margin-top:10%">
+      <p>Hi , the page you are looking for doesnt exists. </p>
+      Please click <a href="/phpframework/">HERE</a> to return 
+    </div>
+
+
+</body>
+</html>
